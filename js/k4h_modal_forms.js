311625c1bb491541e5ead5f21895f5837ea68432
TJ Moyer <tjmoyer@jhu.edu>
2017-02-09 16:20:36 -0500

    added new module k4h_modal_forms to handle forwarding to a resource when a selected modal is closed

---
+++ js/k4h_modal_forms.js
@@ -0,0 +1,9 @@
+jQuery('#meaningful').click(function() {
+  jQueyr.ModalFormsPopup.close();
+  window.location.href = 'http://google.com';
+});
+
+document.querySelector('#meaningful').onclick = function () {
+  
+
+};
